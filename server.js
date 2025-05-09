68d439ce616b97594316eaec6dbcb9cb367acb78
Kumar Akshat <kakshat247@gmail.com>
2025-05-09 11:13:41 +0530

    minor change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv"
 import Razorpay from "razorpay";
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
